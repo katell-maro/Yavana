a74f4580b761db6435d03697bef6d800cdac8ac2
Unknown <katell.maro@gmail.com>
2019-01-22 19:01:00 +0100

    [Config] Premier commit

---
+++ app/src/androidTest/java/com/katell_maro/yavana/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.katell_maro.yavana;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
